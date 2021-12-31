2c08009c3d9786eaa0a27749322e6f7d05796b36
Young June Park <65703011+pyj9748@users.noreply.github.com>
2022-01-01 00:36:18 +0900

    famousSaying

---
+++ famousSaying/famousSaying/Quote.swift
@@ -0,0 +1,14 @@
+//
+//  Quote.swift
+//  famousSaying
+//
+//  Created by young june Park on 2021/12/30.
+//
+
+import Foundation
+
+struct Quote{
+    let contents: String
+    let name: String
+    
+}
