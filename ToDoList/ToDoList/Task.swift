e6d2b500bca38748b835c97b5d082055aaea7f18
Young June Park <65703011+pyj9748@users.noreply.github.com>
2022-01-01 14:31:39 +0900

    ToDoList

---
+++ ToDoList/ToDoList/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  ToDoList
+//
+//  Created by young june Park on 2022/01/01.
+//
+
+import Foundation
+
+struct Task {
+    
+    var title : String
+    var done : Bool
+    
+    
+}
