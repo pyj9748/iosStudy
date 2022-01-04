5a39b4c58713bf3e5f5e0e8aa5dee9cd07932498
Young June Park <65703011+pyj9748@users.noreply.github.com>
2022-01-05 00:43:14 +0900

    Weather

---
+++ Weather/Weather/ErrorMessage.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorMessage.swift
+//  Weather
+//
+//  Created by young june Park on 2022/01/05.
+//
+
+import Foundation
+
+struct ErrorMessage : Codable{
+    let message: String
+    
+}
